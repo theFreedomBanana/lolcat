61dda8a76a81cf5d9dc3298ce45f1607bdc6148d
theFreedomBanana <benjamin.mahmoudi@gmail.com>
2015-12-10 20:31:14 +0100

    adds model yt_search

---
+++ app/models/yt_search.rb
@@ -0,0 +1,13 @@
+class YtSearch
+  def initialize(order, page_token = "")
+    @response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&order=#{order}&pageToken=#{page_token}&q=cats%7Clolcat%7Cfunnycats&type=video&videoEmbeddable=true&key=#{ENV["YT_API_KEY"]}")
+    @next_page = @response["nextPageToken"] 
+    @prev_page = @response["prevPageToken"]
+  end
+
+  attr_reader :response, :next_page, :prev_page
+
+  def [](key)
+    self.response["items"][key]
+  end
+end
