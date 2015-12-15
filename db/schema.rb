690b7b689731b27b9a5355e7b06689b4cf52efa2
theFreedomBanana <benjamin.mahmoudi@gmail.com>
2015-12-15 10:17:25 +0100

    adds relationship between User and Favorite models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213222438) do
+ActiveRecord::Schema.define(version: 20151215090215) do
 
   create_table "favorites", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151213222438) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
